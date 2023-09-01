37ec829060d7a9887dfb34019f941c9754a7eaa1
Ryan O'Donnell <rpodonnell2012@gmail.com>
2023-09-01 12:54:27 -0400

    Update TalonTestSpeedController.ino
    Fixed bugs and stupid code errors. Added some documentation

--- TalonTestSpeedController/TalonTestSpeedController.ino
+++ TalonTestSpeedController/TalonTestSpeedController.ino
@@ -4,13 +4,13 @@ Servo talonSRX;
 
 int power = 25; //In percent from 0 to 100
 
-int talonPWMPin = 9;
+int talonPWMPin = 9; //Pin the yellow can bus wire is connected to
 
 void setup() {
-  myservo.attach(talonPWMPin);  // attaches the servo on pin 9 to the servo object
+  talonSRX.attach(talonPWMPin);  // attaches the server object for the talon to the talonPWMPin pin
 }
 
 void loop() {
-  int rawPower = map(power, 0, 1023, 0, 100);
-  myservo.write(rawPower);
+  int rawPower = map(power, 0, 1023, 0, 100); //Converts percent out put to raw output for the motor
+  talonSRX.write(rawPower); //writes the power to the talon srx
 }
