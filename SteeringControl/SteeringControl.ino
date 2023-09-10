87d0a9ac5951edabff69d8fa29cc6a66a8703f80
Ryan O'Donnell <rpodonnell2012@gmail.com>
2023-09-10 18:23:45 -0400

    Updated code

--- SteeringControl/SteeringControl.ino
+++ SteeringControl/SteeringControl.ino
@@ -7,15 +7,15 @@ double encoderAngle = 0.0;
 double targetEncoderAngle = 0.0;
 
 int talonPWMPin = 9; //Pin the yellow can bus wire is connected to
-int encoderPWMPin = 0; //Pin the absolute encoder is pluged into
+int encoderPWMPin = 3; //Pin the absolute encoder is pluged into
 
 //PID
-int iterationTime = 50;
-double pastError = 0.0;
-double pastInt = 0.0;
-double Kp = 1.0;
-double Ki = 0.1*(1/iterationTime;
-double Kd = 0.0;
+// int iterationTime = 50;
+// double pastError = 0.0;
+// double pastInt = 0.0;
+// double Kp = 1.0;
+// double Ki = 0.1*(1/iterationTime;
+// double Kd = 0.0;
 
 void setup() {
   talonSRX.attach(talonPWMPin);  // attaches the server object for the talon to the talonPWMPin pin
@@ -23,33 +23,46 @@ void setup() {
 }
 
 void loop() {
-  setMotorPower(pidPowerToAngle(getAngle(), targetEncoderAngle));
-  delay(iterationTime);
+  // setMotorPower(pidPowerToAngle(getAngle(), targetEncoderAngle));
+  // delay(iterationTime);
+  Serial.println(GetPWM(encoderPWMPin));//
 }
 
-int pidPowerToAngle(double angle, double targetAngle){
-  double error = targetAngle-angle;
-  double i = pastInt + error * iterationTime;
-  double d = (error-pastError)/iterationTime;
-  double out = Kp*error+Ki*i+Kd*d;
-  pastError = error;
-  pastInt = i;
-  int ret = constrain(int(out),-80,80);
-  Serial.print(ret);
-  Serial.print(" - ");
-  Serial.print(error);
-  Serial.print(" - ");
-  Serial.println(angle);
-  return ret;
-}
+// int pidPowerToAngle(double angle, double targetAngle){
+//   double error = targetAngle-angle;
+//   double i = pastInt + error * iterationTime;
+//   double d = (error-pastError)/iterationTime;
+//   double out = Kp*error+Ki*i+Kd*d;
+//   pastError = error;
+//   pastInt = i;
+//   int ret = constrain(int(out),-80,80);
+//   Serial.print(ret);
+//   Serial.print(" - ");
+//   Serial.print(error);
+//   Serial.print(" - ");
+//   Serial.println(angle);
+//   return ret;
+// }
 
-double getAngle(){
-  //Using Encoder magic to get the absolute angle of the steering column
-  // return map(analogRead(A0), 0, 1023, -180, 180); //temp test code
-  return encoderAngle;
-}
+// long getAngle(){
+//   //Using Encoder magic to get the absolute angle of the steering column
+//   // return map(analogRead(A0), 0, 1023, -180, 180); //temp test code
+//   return GetPWM(encoderPWMPin);
+// }
 
-void setMotorPower(int motorPower){
-  int rawPower = map(motorPower, -100, 100, 0, 1023); //Converts percent out put to raw output for the motor
-  talonSRX.write(rawPower); //writes the power to the talon srx
+// void setMotorPower(int motorPower){
+//   int rawPower = map(motorPower, -100, 100, 0, 1023); //Converts percent out put to raw output for the motor
+//   talonSRX.write(rawPower); //writes the power to the talon srx
+// }
+
+long GetPWM(int pin)
+{
+  unsigned long highTime = pulseIn(pin, HIGH, 50000UL);  // 50 millisecond timeout
+  unsigned long lowTime = pulseIn(pin, LOW, 50000UL);  // 50 millisecond timeout
+
+  // pulseIn() returns zero on timeout
+  // if (highTime == 0 || lowTime == 0)
+  //   return (long)digitalRead(pin) ? 100.0 : 0.0;  // HIGH == 100%,  LOW = 0%
+
+  return (100*highTime) / (highTime + lowTime));  // highTime as percentage of total cycle time
 }
