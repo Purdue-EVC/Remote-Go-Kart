89966579d3c6a7eba6d6fe7dcfe254c15d4a85ad
Ryan O'Donnell <rpodonnell2012@gmail.com>
2023-09-11 19:10:02 -0400

    Update SteeringControl.ino

--- SteeringControl/SteeringControl.ino
+++ SteeringControl/SteeringControl.ino
@@ -1,68 +1,97 @@
 #include <Servo.h>
 
-Servo talonSRX;
+Servo motor;
 
-int power = 25; //In percent from 0 to 100
 double encoderAngle = 0.0;
 double targetEncoderAngle = 0.0;
+const int deadRangeLow = 84;   // Low filter range
+const int deadRangeHigh = 94;  // High filter range
 
-int talonPWMPin = 9; //Pin the yellow can bus wire is connected to
-int encoderPWMPin = 3; //Pin the absolute encoder is pluged into
+int ch1Pin = 3;  //RC Radio Pin
+int encoderPWMPin = 11;
+int motorPin = 8;
 
-//PID
-// int iterationTime = 50;
-// double pastError = 0.0;
-// double pastInt = 0.0;
-// double Kp = 1.0;
-// double Ki = 0.1*(1/iterationTime;
-// double Kd = 0.0;
+long prevRawSteering = 0;
+long currentRawSteering = 0;
+long filteredSteering = 0;
+long motorPower = 90;
+
+float weight = 0.05;
+int filterAvgNum = 5;
+double avg = 0;
+
+// PID
+int iterationTime = 50;
+double pastError = 0.0;
+double pastInt = 0.0;
+double Kp = .5;
+double Ki = 0.1*(1/iterationTime);
+double Kd = 0.0;
 
 void setup() {
-  talonSRX.attach(talonPWMPin);  // attaches the server object for the talon to the talonPWMPin pin
-  Serial.begin(9600);
+  motor.attach(motorPin);
+  // Serial.begin(9600);
+}
+
+void serialOut(int ret, double error, double angle){
+  Serial.print(ret);
+  Serial.print(" - ");
+  Serial.print(error);
+  Serial.print(" - ");
+  Serial.println(angle);
 }
 
 void loop() {
-  // setMotorPower(pidPowerToAngle(getAngle(), targetEncoderAngle));
-  // delay(iterationTime);
-  Serial.println(GetPWM(encoderPWMPin));//
+  prevRawSteering = currentRawSteering;
+  currentRawSteering = approxRollingAverage(prevRawSteering, getPWM(encoderPWMPin));
+  motor.write(pidPowerToAngle(getAngle(), getSteeringPower(getFilteredSteering(currentRawSteering, prevRawSteering, weight))));
 }
 
-// int pidPowerToAngle(double angle, double targetAngle){
-//   double error = targetAngle-angle;
-//   double i = pastInt + error * iterationTime;
-//   double d = (error-pastError)/iterationTime;
-//   double out = Kp*error+Ki*i+Kd*d;
-//   pastError = error;
-//   pastInt = i;
-//   int ret = constrain(int(out),-80,80);
-//   Serial.print(ret);
-//   Serial.print(" - ");
-//   Serial.print(error);
-//   Serial.print(" - ");
-//   Serial.println(angle);
-//   return ret;
-// }
+int pidPowerToAngle(double angle, double targetAngle){
+  double error = targetAngle-angle;
+  double i = pastInt + error * iterationTime;
+  double d = (error-pastError)/iterationTime;
+  double out = Kp*error+Ki*i+Kd*d;
+  pastError = error;
+  pastInt = i;
+  int ret = constrain(int(out),-80,80);//
+  ret = map(ret,-80,80, -10, 10);
+  ret = map(ret,-10,10, -40, 40);
+  // serialOut(ret, error, angle);
+  return ret;
+}
 
-// long getAngle(){
-//   //Using Encoder magic to get the absolute angle of the steering column
-//   // return map(analogRead(A0), 0, 1023, -180, 180); //temp test code
-//   return GetPWM(encoderPWMPin);
-// }
+long getAngle(){
+  //Using Encoder magic to get the absolute angle of the steering column
+  return map(getPWM(11), 0, 1023, -180, 180);
+}
 
-// void setMotorPower(int motorPower){
-//   int rawPower = map(motorPower, -100, 100, 0, 1023); //Converts percent out put to raw output for the motor
-//   talonSRX.write(rawPower); //writes the power to the talon srx
-// }
+long getPWM(int pin) {
+  unsigned long highTime = pulseIn(pin, HIGH, 50000UL);
+  unsigned long lowTime = pulseIn(pin, LOW, 50000UL);
+  return highTime;
+}
 
-long GetPWM(int pin)
-{
-  unsigned long highTime = pulseIn(pin, HIGH, 50000UL);  // 50 millisecond timeout
-  unsigned long lowTime = pulseIn(pin, LOW, 50000UL);  // 50 millisecond timeout
+long getFilteredSteering(long currentRawSteering, long prevRawSteering, float weight) {
+  return weight * currentRawSteering + (1 - weight) * prevRawSteering - 1;
+}
 
-  // pulseIn() returns zero on timeout
-  // if (highTime == 0 || lowTime == 0)
-  //   return (long)digitalRead(pin) ? 100.0 : 0.0;  // HIGH == 100%,  LOW = 0%
+long getSteeringPower(int raw) {
+  long pow = constrain(map(raw, 1190, 1990, 0, 180), 0, 180); 
+  if (pow < deadRangeLow) {
+    return pow;
+  }
+  else if (pow > deadRangeHigh) {
+    return pow;
+  }
+  else {
+    return 92;
+  }
+}
 
-  return (100*highTime) / (highTime + lowTime));  // highTime as percentage of total cycle time
+double approxRollingAverage (double avg, double new_sample) {
+    avg -= avg / filterAvgNum;
+    avg += new_sample / filterAvgNum;
+
+    return avg;
 }
